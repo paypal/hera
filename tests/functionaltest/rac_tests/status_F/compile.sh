987bf8cc6e96d1c88ab384b840c346bbbba9e072
stephanie <stvu@paypal.com>
2020-06-05 16:35:55 -0700

    Added  Rac test cases

--- tests/functionaltest/rac_tests/status_F/compile.sh
+++
@@ -1,1 +0,0 @@
-go test -c ../../../../../../../github.com/paypal/hera/tests/functionaltest/rac_tests/status_F
---
+++ tests/functionaltest/rac_tests/status_R_Tcurr_GT_Tsch/compile.sh
@@ -0,0 +1,1 @@
+go test -c ../../../../../../../github.com/paypal/hera/tests/functionaltest/rac_tests/status_R_Tcurr_GT_Tsch
