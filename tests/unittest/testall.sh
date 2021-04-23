b71af1a948a0dd3791d36730d11196654e32ffa6
Kenneth Kang <kkang@paypal.com>
2021-04-23 16:37:48 -0700

    avoid failover tests that use multiple mysql DBs

--- tests/unittest/testall.sh
+++ tests/unittest/testall.sh
@@ -1,15 +1,12 @@
-for d in `ls -F tests/unittest | grep /$ | sed -e "s,/,," | egrep -v '(testutil|rac_maint|mysql_direct)'`
+for d in `ls -F tests/unittest | grep /$ | sed -e "s,/,," | egrep -v '(testutil|rac_maint|mysql_direct|failover)'`
 do 
     echo ==== $d
     pushd tests/unittest/$d 
     cp /home/runner/go/bin/mysqlworker .
-    sha384sum mysqlworker /home/runner/work/hera/hera/tests/unittest/backoff_recycle/mysqlworker
     rm -f *.log 
     $GOROOT/bin/go test -c github.com/paypal/hera/tests/unittest/$d 
-    sha384sum mysqlworker /home/runner/work/hera/hera/tests/unittest/backoff_recycle/mysqlworker
     ./$d.test 
     rv=$?
-    sha384sum mysqlworker /home/runner/work/hera/hera/tests/unittest/backoff_recycle/mysqlworker
     grep -E '(FAIL|PASS)' -A1 *.log
     if [ 0 != $rv ]
     then
@@ -17,5 +14,6 @@ do
         popd
         exit $rv
     fi
+    rm -f *.log 
     popd
 done
