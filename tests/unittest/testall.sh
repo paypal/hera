47762777ce75efb65a9bec6df3eb83f0c64e41e9
Kenneth Kang <kkang@paypal.com>
2021-04-23 16:01:14 -0700

    more logs on failures

--- tests/unittest/testall.sh
+++ tests/unittest/testall.sh
@@ -1,9 +1,15 @@
 for d in `ls -F $GOPATH/src/github.com/paypal/hera/tests/unittest | grep /$ | egrep -v '(testutil|rac_maint|mysql_direct)'`
 do 
-    $GOROOT/bin/go test github.com/paypal/hera/tests/unittest/$d 
+    echo ==== $d
+    cd $GOPATH/src/github.com/paypal/hera/tests/unittest/$d 
+    rm -f *.log 
+    $GOROOT/bin/go test -c github.com/paypal/hera/tests/unittest/$n 
+    ./$n.test 
     rv=$?
+    grep -E '(FAIL|PASS)' -A1 *.log
     if [ 0 != $rv ]
     then
+        grep ^ *.log
         exit $rv
     fi
 done
