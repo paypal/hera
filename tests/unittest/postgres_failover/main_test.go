1a9c6c8ee1f8de287a198b055c2e77897b8812d5
venkatsridhar95 <vesridhar@paypal.com>
2021-10-12 13:55:24 -0700

    Failover changes for postgres worker

--- tests/unittest/postgres_failover/main_test.go
+++ tests/unittest/postgres_failover/main_test.go
@@ -9,7 +9,7 @@ import (
 	"testing"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/lib/pq"
 	"github.com/paypal/hera/tests/unittest/testutil"
 	"github.com/paypal/hera/utility/logger"
 )
@@ -20,31 +20,21 @@ var tableName string
 func cfg() (map[string]string, map[string]string, testutil.WorkerType) {
 
 	appcfg := make(map[string]string)
-	appcfg["x-mysql"] = "manual" // disable test framework spawning mysql server
 	// best to chose an "unique" port in case golang runs tests in paralel
-	appcfg["bind_port"] = "31002"
+	appcfg["bind_port"] = "32002"
 	appcfg["log_level"] = "5"
 	appcfg["log_file"] = "hera.log"
 	appcfg["sharding_cfg_reload_interval"] = "0"
-	appcfg["rac_sql_interval"] = "2"
+	appcfg["rac_sql_interval"] = "0"
 	appcfg["db_heartbeat_interval"] = "3"
 
 	opscfg := make(map[string]string)
 	opscfg["opscfg.default.server.max_connections"] = "3"
 	opscfg["opscfg.default.server.log_level"] = "5"
 
-	appcfg["child.executable"] = "mysqlworker"
+	appcfg["child.executable"] = "postgresworker"
 
-	return appcfg, opscfg, testutil.MySQLWorker
-}
-
-func before() error {
-	pfx := os.Getenv("MGMT_TABLE_PREFIX")
-	if pfx == "" {
-		pfx = "hera"
-	}
-	tableName = pfx + "_maint"
-	return nil
+	return appcfg, opscfg, testutil.PostgresWorker
 }
 
 var ip1 string
@@ -52,25 +42,11 @@ var ip2 string
 var dbName = "failovertestdb"
 
 func TestMain(m *testing.M) {
-	// startup two mysql DBs
-	ip1 = testutil.MakeDB("mysql33",dbName,testutil.MySQL)
-	ip2 = testutil.MakeDB("mysql44",dbName,testutil.MySQL)
-	os.Setenv("TWO_TASK", "tcp("+ip1+":3306)/"+dbName+"?timeout=11s||tcp("+ip2+":3306)/"+dbName+"?timeout=11s")
-
-	/*
-	for {
-		conn, err := net.Dial("tcp", ip2+":3306")
-		if err != nil {
-			time.Sleep(1 * time.Second)
-			logger.GetLogger().Log(logger.Warning, "waiting for mysql server to come up")
-			continue
-		} else {
-			conn.Close()
-			break
-		}
-	} // */
-
-	os.Exit(testutil.UtilMain(m, cfg, before))
+	// startup two postgres DBs
+	ip1 = testutil.MakeDB("postgres33",dbName,testutil.PostgreSQL)
+	ip2 = testutil.MakeDB("postgres44",dbName,testutil.PostgreSQL)
+	os.Setenv("TWO_TASK", ip1+"/"+dbName+"?connect_timeout=60&sslmode=disable||"+ip2+"/"+dbName+"?connect_timeout=60&sslmode=disable")
+	os.Exit(testutil.UtilMain(m, cfg, nil))
 }
 
 func TestFailover(t *testing.T) {
@@ -94,20 +70,13 @@ func TestFailover(t *testing.T) {
 	defer conn.Close()
 
 	doCrud(conn, 1, t)
-	/*
-	conn2, err := db.Conn(ctx)
-	if err != nil {
-		logger.GetLogger().Log(logger.Debug, "reacq conn "+err.Error())
-	}
-	doCrud(conn2, 1, t)
-	conn2.Close() //*/
 
 	logger.GetLogger().Log(logger.Debug, "TestFailover taking out first db +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
-        cleanCmd := exec.Command("docker", "stop", "mysql33")
-        cleanCmd.Run()
+	cleanCmd := exec.Command("docker", "stop", "postgres33")
+	cleanCmd.Run()
 	logger.GetLogger().Log(logger.Debug, "TestFailover taken out first db +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
 
-	time.Sleep(8 * time.Second)
+	time.Sleep(90 * time.Second)
 	/* It's easier just to wait for some time instead of trying to flush
 	old connections */
 	logger.GetLogger().Log(logger.Debug, "TestFailover flush wait done +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
@@ -134,93 +103,64 @@ func TestFailover(t *testing.T) {
 	logger.GetLogger().Log(logger.Debug, "TestFailover done +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
 
 	// clean up
-        cleanCmd = exec.Command("docker", "start", "mysql33")
-        cleanCmd.Run()
+	cleanCmd = exec.Command("docker", "rm", "postgres33")
+	cleanCmd.Run()
+	cleanCmd = exec.Command("docker", "stop", "postgres44")
+	cleanCmd.Run()
+	cleanCmd = exec.Command("docker", "rm", "postgres44")
+	cleanCmd.Run()
+}
 
+func commit(conn *sql.Conn, t* testing.T) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	tx, err := conn.BeginTx(ctx, nil)
+	if err != nil {
+		t.Fatalf("Error begin tx %s\n", err.Error())
+	}
+	err = tx.Commit()
+	if err != nil {
+		t.Fatalf("Error commit %s\n", err.Error())
+	}
+}
+
+func rollback(conn *sql.Conn, t* testing.T) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	tx, err := conn.BeginTx(ctx, nil)
+	if err != nil {
+		t.Fatalf("Error begin tx %s\n", err.Error())
+	}
+	err = tx.Rollback()
+	if err != nil {
+		t.Fatalf("Error rollback %s\n", err.Error())
+	}
 }
 
 func doCrud(conn *sql.Conn, id int, t* testing.T) (bool) {
 
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
-	//note := time.Now().Format("test note 2006-01-02j15:04:05.000 failover")
 	stmt, err := conn.PrepareContext(ctx, "drop table test_failover")
 	if err != nil {
+		rollback(conn, t)
 		return false
 	}
-	//noTable := false
+
 	_,err = stmt.Exec()
 	if err != nil {
-		//noTable = true
+		rollback(conn, t)
 	}
 	// ignore errors since table might not exist
 
 	stmt, err = conn.PrepareContext(ctx, "create table test_failover ( id int, note varchar(55) )")
 	if err != nil {
+		rollback(conn, t)
 		return false
 	}
 	_,err = stmt.Exec()
 	if err != nil {
+		rollback(conn, t)
 		t.Fatalf("create table had issue %s",err.Error())
 	}
-	// ignore errors since table may already exist
-
-	/*
-	// not using txn since mysql
-	stmt, err = conn.PrepareContext(ctx, "insert into test_failover ( id , note ) values ( ?, ? )")
-	if err != nil {
-		t.Fatalf("Error preparing test (insert table) %s\n", err.Error())
-	}
-	_, err = stmt.Exec(id, note)
-	if err != nil {
-		t.Fatalf("Error exec test (insert table) %s\n", err.Error())
-	}
-
-	stmt, err = conn.PrepareContext(ctx, "insert into test_failover (id , note ) values ( ?, ? )")
-	if err != nil {
-		t.Fatalf("Error prep test (insert neg-id table) %s\n", err.Error())
-	}
-	_, err = stmt.Exec(-id, note)
-	if err != nil {
-		t.Fatalf("Error exec test (insert neg-id table) %s\n", err.Error())
-	}
-
-	/*
-	stmt, err = conn.PrepareContext(ctx, "select note from test_failover where id = ?")
-	if err != nil {
-		t.Fatalf("Error preparing test (sel table) %s\n", err.Error())
-	}
-        rows, _ := stmt.Query(id)
-        if !rows.Next() {
-                t.Fatalf("Expected 1 row")
-        }
-        var str_val sql.NullString
-        err = rows.Scan(&str_val)
-	if err != nil {
-		t.Fatalf("Error preparing test (sel scan table) %s\n", err.Error())
-	}
-	if !str_val.Valid {
-		t.Fatalf("null str")
-	}
-	if str_val.String != note {
-		t.Fatalf("data corrupt "+note+" dbHas:"+ str_val.String)
-	}
-
-        rows.Close()
-        stmt.Close()
-	// */
-
-
-	/*
-	stmt, err := conn.PrepareContext(ctx, "delete from test_failover where id = ?")
-	if err != nil {
-		t.Fatalf("Error preparing test (del table) %s\n", err.Error())
-	}
-	_, err = stmt.Exec(id)
-	if err != nil {
-		t.Fatalf("Error exec test (del table) %s\n", err.Error())
-	}
-	// */
+	commit(conn, t)
 	return true
 }
-
