745bb52c21f506166424aa41372cac98de218f10
Kenneth Kang <kkang@paypal.com>
2022-06-09 15:52:31 -0700

    printing failed suite and test, exit with finalResult

--- tests/functionaltest/runall.sh
+++ tests/functionaltest/runall.sh
@@ -7,6 +7,7 @@ then
 fi
 $GOROOT/bin/go install github.com/paypal/hera/worker/mysqlworker
 suites="bind_eviction_tests strandedchild_tests coordinator_tests saturation_tests adaptive_queue_tests rac_tests sharding_tests"
+finalResult=0
 for suite in $suites
 do 
   for d in `ls -F $GOPATH/src/github.com/paypal/hera/tests/functionaltest/$suite | grep /$ | sed -e 's,/,,' | egrep -v '(testutil|no_shard_no_error|set_shard_id_wl|reset_shard_id_wl)'`
@@ -18,11 +19,13 @@ do
       rv=$?
       if [ 0 != $rv ]
       then
-         echo failing
+         echo failing $suite $d
          grep ^ *.log
+         finalResult=$rv
 #        exit $rv
       fi
       popd
       sleep 10
   done
 done
+exit $finalResult
