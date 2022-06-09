20be7c6e855fafc97584ce15685604157218943e
Kenneth Kang <kkang@paypal.com>
2022-06-09 15:02:27 -0700

    manual github actions workflow for functional test

--- tests/functionaltest/runall.sh
+++ tests/functionaltest/runall.sh
@@ -1,15 +1,28 @@
+if [ x$GOPATH = x ]
+then
+  # when running in github actions workflow
+  export GOPATH=$PWD/testrun
+  mkdir -p testrun/src/github.com/paypal
+  ln -s $PWD testrun/src/github.com/paypal/hera
+fi
+$GOROOT/bin/go install github.com/paypal/hera/worker/mysqlworker
 suites="bind_eviction_tests strandedchild_tests coordinator_tests saturation_tests adaptive_queue_tests rac_tests sharding_tests"
 for suite in $suites
 do 
-  for d in `ls -F $GOPATH/src/github.com/paypal/hera/tests/functionaltest/$suite | grep /$ | egrep -v '(testutil|no_shard_no_error|set_shard_id_wl|reset_shard_id_wl)'`
+  for d in `ls -F $GOPATH/src/github.com/paypal/hera/tests/functionaltest/$suite | grep /$ | sed -e 's,/,,' | egrep -v '(testutil|no_shard_no_error|set_shard_id_wl|reset_shard_id_wl)'`
   do 
-      $GOROOT/bin/go test github.com/paypal/hera/tests/functionaltest/$suite/$d 
+      pushd $GOPATH/src/github.com/paypal/hera/tests/functionaltest/$suite/$d
+      cp $GOPATH/bin/mysqlworker .
+      $GOROOT/bin/go test -c .
+      ./$d.test
       rv=$?
       if [ 0 != $rv ]
       then
          echo failing
+         grep ^ *.log
 #        exit $rv
       fi
+      popd
       sleep 10
   done
 done
