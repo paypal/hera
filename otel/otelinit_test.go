abce22bc85cf3531fbee822784799466454b5ad0
Alwin Joseph <alwjoseph@paypal.com>
2022-08-03 16:03:41 -0700

    Added http otel mock collector for testing and verification

--- otel/otelinit_test.go
+++ otel/otelinit_test.go
@@ -11,9 +11,16 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric/global"
 	"go.opentelemetry.io/otel/metric/instrument"
+	v1 "go.opentelemetry.io/proto/otlp/metrics/v1"
 )
 
 func TestCounter(t *testing.T) {
+
+	mc := runMockCollector(t, mockCollectorConfig{
+		Port: 4318,
+	})
+	defer mc.MustStop(t)
+
 	defer initMetricProvider()()
 
 	ctx := context.Background()
@@ -35,9 +42,26 @@ func TestCounter(t *testing.T) {
 		fmt.Println("Counter..it:==>" + strconv.Itoa(i))
 	}
 
+	// wait for pusher
+	time.Sleep(6 * time.Second)
+
+	v1m := mc.GetMetrics()
+
+	for _, v1mElement := range v1m {
+		var val *v1.NumberDataPoint_AsInt = v1mElement.GetSum().DataPoints[0].Value.(*v1.NumberDataPoint_AsInt)
+		fmt.Println(val.AsInt)
+		fmt.Println("--------------------------")
+
+	}
+
 }
 
 func TestVariableDimentionCounter(t *testing.T) {
+	mc := runMockCollector(t, mockCollectorConfig{
+		Port: 4318,
+	})
+	defer mc.MustStop(t)
+
 	defer initMetricProvider()()
 
 	ctx := context.Background()
@@ -53,11 +77,13 @@ func TestVariableDimentionCounter(t *testing.T) {
 		attribute.String("client", "cli"),
 	}
 
-	for i := 1; i <= 50; i++ {
+	for i := 1; i <= 5; i++ {
 
 		min := 0
 		max := 50
-		commonLabelsLocal := append(commonLabels, attribute.String("sqlhash", strconv.Itoa(rand.Intn(max-min)+min)))
+		sqlHash := strconv.Itoa(rand.Intn(max-min) + min)
+		fmt.Println("sqlHash:==>", sqlHash)
+		commonLabelsLocal := append(commonLabels, attribute.String("sqlhash", sqlHash))
 
 		requestCount.Add(ctx, 1, commonLabelsLocal...)
 		time.Sleep(1 * time.Second)
@@ -67,6 +93,11 @@ func TestVariableDimentionCounter(t *testing.T) {
 }
 
 func TestHistogram(t *testing.T) {
+	mc := runMockCollector(t, mockCollectorConfig{
+		Port: 4318,
+	})
+	defer mc.MustStop(t)
+
 	defer initMetricProvider()()
 
 	ctx := context.Background()
@@ -83,7 +114,7 @@ func TestHistogram(t *testing.T) {
 		attribute.String("client", "cli"),
 	}
 
-	for i := 1; i <= 50; i++ {
+	for i := 1; i <= 5; i++ {
 		min := 1
 		max := 15
 		duration := float64(rand.Intn(max-min) + min)
@@ -92,5 +123,41 @@ func TestHistogram(t *testing.T) {
 		time.Sleep(1 * time.Second)
 		fmt.Println("Counter:==>" + strconv.Itoa(i))
 	}
+
+}
+
+func TestGauage(t *testing.T) {
+	mc := runMockCollector(t, mockCollectorConfig{
+		Port: 4318,
+	})
+	defer mc.MustStop(t)
+	defer initMetricProvider()()
+
+	meter := global.Meter("herapoc-demo-client-meter")
+
+	gauge, _ := meter.AsyncInt64().Gauge(
+		"heratest_demo_guage",
+		// instrument.WithUnit("1"),
+		// instrument.WithDescription("TODO"),
+	)
+	min := 1
+	max := 15
+
+	if err := meter.RegisterCallback(
+		[]instrument.Asynchronous{
+			gauge,
+		},
+		func(ctx context.Context) {
+			fmt.Println("Gauge::" + time.Now().String())
+
+			duration := rand.Intn(max-min) + min
+			// debug.PrintStack()
+			gauge.Observe(ctx, int64(duration))
+		},
+	); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(20 * time.Second)
 
 }
