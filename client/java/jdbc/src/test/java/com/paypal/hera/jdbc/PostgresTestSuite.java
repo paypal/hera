315d22dbf0c386ec050f19400e14802b9edf8d4b
nikhil2406 <nikhil2406@gmail.com>
2022-11-26 17:48:46 -0800

    separate jdbc test suite for mysql and postgres. Cleanup of docker images at end.

--- client/java/jdbc/src/test/java/com/paypal/hera/jdbc/PostgresTestSuite.java
+++ client/java/jdbc/src/test/java/com/paypal/hera/jdbc/PostgresTestSuite.java
@@ -1,8 +1,11 @@
 package com.paypal.hera.jdbc;
 
 
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+
+import java.io.IOException;
 
 @RunWith(Suite.class)
 
@@ -12,4 +15,10 @@ import org.junit.runners.Suite;
 })
 
 public class PostgresTestSuite {
+    @AfterClass
+    public static void cleanup() throws IOException, InterruptedException {
+        Runtime.getRuntime().exec("docker stop postgres55").waitFor();
+        Runtime.getRuntime().exec("docker rm postgres55").waitFor();
+        Runtime.getRuntime().exec("killall -ILL mux postgresworker").waitFor();
+    }
 }
