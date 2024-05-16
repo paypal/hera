b9dc4b267b455ff5edc34d4587db5a7738d2d0f8
Rajesh S <samal.rajesh@gmail.com>
2024-05-16 10:12:46 +0530

    changes for simplifying test code for qbb

--- tests/unittest/querybindblocker_ratelimit_table_empty/main_test.go
+++ tests/unittest/querybindblocker_ratelimit_table_empty/main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"os"
@@ -41,35 +40,8 @@ func TestMain(m *testing.M) {
 	os.Exit(testutil.UtilMain(m, cfg, nil))
 }
 
-func executeQuery(t *testing.T, db *sql.DB) {
-	ctx := context.Background()
-	conn, err := db.Conn(ctx)
-	defer conn.Close()
-	if err != nil {
-		t.Fatalf("conn %s", err.Error())
-	}
-
-	tx, _ := conn.BeginTx(ctx, nil)
-	stmt, err := tx.PrepareContext(ctx, "/*qbb_test.find*/select id, note from qbb_test where id=? for update")
-	defer stmt.Close()
-	if err != nil {
-		t.Fatalf("Error prep sel %s\n", err.Error())
-		tx.Rollback()
-	}
-	_, err = stmt.Query(11)
-	if err != nil {
-		t.Fatalf("Failed to execute query with error: %v", err)
-		tx.Rollback()
-	}
-	tx.Commit()
-	stmt.Close()
-	conn.Close()
-}
-
 func TestQueryBindBlockerTableNotExistOrEmpty(t *testing.T) {
 	testutil.RunDML("DROP TABLE IF EXISTS hera_rate_limiter")
-	testutil.RunDML("DROP TABLE IF EXISTS qbb_test")
-	testutil.RunDML("create table qbb_test (id numeric, note varchar(111))")
 
 	logger.GetLogger().Log(logger.Debug, "TestQueryBindBlockerTableNotExistOrEmpty begin +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n")
 
@@ -81,51 +53,14 @@ func TestQueryBindBlockerTableNotExistOrEmpty(t *testing.T) {
 	}
 	db.SetMaxIdleConns(0)
 	defer db.Close()
-	ctx := context.Background()
-	// cleanup and insert one row in the table
-	conn, err := db.Conn(ctx)
-	if err != nil {
-		t.Fatalf("Error getting connection %s\n", err.Error())
-	}
 
-	tx, err := conn.BeginTx(ctx, nil)
-	if err != nil {
-		t.Fatalf("tx %s", err.Error())
-	}
-	stmt, err := tx.PrepareContext(ctx, "/*setup qbb t*/delete from qbb_test")
-	if err != nil {
-		t.Fatalf("prep %s", err.Error())
-	}
-	_, err = stmt.Exec()
-	if err != nil {
-		t.Fatalf("Error preparing test (delete table) %s\n", err.Error())
-	}
-	stmt, err = tx.PrepareContext(ctx, "/*setup qbb t*/insert into qbb_test(id, note) VALUES(?, ?)")
-	if err != nil {
-		t.Fatalf("prep ins %s", err.Error())
-	}
-	_, err = stmt.Exec(11, "eleven")
-	if err != nil {
-		t.Fatalf("Error preparing test (create row in table) %s\n", err.Error())
-	}
-	err = tx.Commit()
-	if err != nil {
-		t.Fatalf("Error commit %s\n", err.Error())
-	}
-	stmt.Close()
-	conn.Close()
-
-	time.Sleep(3 * time.Second)
-
-	executeQuery(t, db)
-	time.Sleep(3 * time.Second)
+	time.Sleep(6 * time.Second)
 	if testutil.RegexCountFile("loading query bind blocker: SQL error: Error 1146", "hera.log") == 0 {
 		t.Fatalf("expected to see table 'hera_rate_limiter' doesn't exist error")
 	}
 
 	testutil.RunDML("create table hera_rate_limiter (herasqlhash numeric not null, herasqltext varchar(4000) not null, bindvarname varchar(200) not null, bindvarvalue varchar(200) not null, blockperc numeric not null, heramodule varchar(100) not null, end_time numeric not null, remarks varchar(200) not null)")
 	time.Sleep(15 * time.Second)
-	executeQuery(t, db)
 	if testutil.RegexCountFile("Loaded 0 sqlhashes, 0 entries, query bind blocker entries", "hera.log") == 0 {
 		t.Fatalf("expected to 0 entries from hera_rate_limiter table")
 	}
